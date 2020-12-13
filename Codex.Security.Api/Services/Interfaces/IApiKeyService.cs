33afdd6dcbab8470bcc5bc3773ef340284e453e5
NitrofCG <thetyne@live.fr>
2020-12-13 22:26:26 +0100

    Add ApiKeyService

---
+++ Codex.Security.Api/Services/Interfaces/IApiKeyService.cs
@@ -0,0 +1,19 @@
+﻿using Codex.Models.Security;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Codex.Security.Api.Services.Interfaces
+{
+    public interface IApiKeyService
+    {
+        Task<List<ApiKey>> FindAllAsync(ApiKeyCriteria apiKeyCriteria);
+
+        Task<ApiKey?> FindOneAsync(string id);
+
+        Task<ApiKey> CreateAsync(ApiKey apiKey);
+
+        Task<ApiKey?> UpdateAsync(ApiKey apiKey);
+
+        Task DeleteAsync(string apiKeyId);
+    }
+}
